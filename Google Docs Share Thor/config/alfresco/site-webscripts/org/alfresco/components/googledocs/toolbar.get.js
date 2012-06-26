882493cd04a54cde3affd1cf71ff8dd0d163db5e
Will Abson <will.abson@gmail.com>
2012-06-26 12:04:19 +0000

    Google Docs toolbar component now checks for presence of cm:versionable aspect on the node being edited in order to decide whether to show the full version dialogue when the save button is clicked.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/integrations/GoogleDocs/BRANCHES/DEV@38355 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

--- Google Docs Share Thor/config/alfresco/site-webscripts/org/alfresco/components/googledocs/toolbar.get.js
+++ Google Docs Share Thor/config/alfresco/site-webscripts/org/alfresco/components/googledocs/toolbar.get.js
@@ -1,9 +1,28 @@
-/**
+<import resource="classpath:/alfresco/templates/org/alfresco/import/alfresco-util.js">
+
+/*
  * Is the node versioned?
  */
 function isVersioned()
 {
+   AlfrescoUtil.param('nodeRef');
    // TODO: Data webscript call to return whether the item is versioned or not
+   //var aspectsResp = remote.call("slingshot/doclib/aspects/node/" + model.nodeRef.replace(":/", ""));
+   var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, null);
+   var aspects = documentDetails.item.node.aspects;
+   for (var i = 0; i < aspects.length; i++)
+   {
+      if (aspects[i] == "cm:versionable")
+      {
+         return true;
+      }
+   }
+   return false;
 }
 
-model.isVersioned = false;
+function main()
+{
+   model.isVersioned = isVersioned();
+}
+
+main();
